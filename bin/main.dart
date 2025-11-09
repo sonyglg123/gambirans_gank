d623eeb002980f3670b4fd09140865b30d1d5307
sonyglg123 <sonyglg123@gmail.com>
2025-11-09 17:15:55 +0700

    Add files via upload

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,13 @@
+import 'package:main/queries/select_query.dart';
 
+void main() {
+  print('SQL Query Builder - Week 1: Polymorphism & Abstraction');
+  print('====================================================');
+
+  final query = SelectQuery();
+  query.from('users');
+  query.where('id', '=', 12);
+
+  final sql = query.build();
+  print('Generated SQL: $sql');
+}
